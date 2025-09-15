729197e1a036d04740fd6957f0f5e1d1128a533a
Jamir <jamirvega@gmail.com>
2025-09-15 14:34:09 -0400

    Docker compose setup , data table creation

---
+++ data /tables.sql
@@ -0,0 +1,38 @@
+/*
+    To populate the database for base entries
+    Use the following command
+    \i data/tables.sql
+    */
+
+
+DROP TABLE IF EXISTS entry;
+DROP TABLE IF EXISTS to_dos;
+
+CREATE TABLE entry(
+    entry_id SERIAL PRIMARY KEY,
+    date DATE NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    content TEXT NOT NULL
+);
+
+CREATE TABLE to_dos (
+    to_do_id SERIAL PRIMARY KEY,
+    entry_id int REFERENCES entries(entry_id),
+    task TEXT NOT NULL,
+    is_completed BOOLEAN
+);
+
+INSERT INTO entries (date,title, content) VALUES
+('2025-09-12',"Test 1","Foo Bar Bar Foo"),
+('2025-09-09',"Test 2","Foo Bar Bar Foo"),
+('2025-08-28',"Test 3","Foo Bar Bar Foo"),
+('2025-08-26',"Test 4","Foo Bar Bar Foo");
+
+INSERT INTO to_dos (entry_id,task,is_completed) VALUES
+(1,"Ipsum Dolor","TRUE"),
+(1,"Ipsum Dolor","FALSE"),
+(2,"Ipsum Dolor","FALSE"),
+(3,"Ipsum Dolor","TRUE"),
+(3,"Ipsum Dolor","FALSE"),
+(4,"Ipsum Dolor","TRUE"),
+(4,"Ipsum Dolor","FALSE");
